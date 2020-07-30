27efaa3e22a4a7026dafd114f41c920ab6ead786
Mercurius-0227 <10rladmsql@naver.com>
2020-07-31 00:39:54 +0900

    clear

---
+++ _11650_wjdfuf.py
@@ -0,0 +1,15 @@
+#def clean(a,b,t):
+#    for i in range(t):
+#        num[i][2]=sum(a[i],b[i])
+     
+
+t=int(input())
+num=[]
+for i in range(t):
+    num=list(input().split())
+    num[i]=int(num[i])
+
+print(num)
+
+
+#clean(num,t)

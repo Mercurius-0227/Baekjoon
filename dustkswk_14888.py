5705eabeb34e878ba05c4fab2bb856d306d741c1
Mercurius-0227 <10rladmsql@naver.com>
2020-07-31 00:33:33 +0900

    clear?

---
+++ dustkswk_14888.py
@@ -0,0 +1,26 @@
+#14888번. 연산자 끼워넣기
+#2020.07.30
+
+def make_sik(num,sik):
+    list_dustkswk=[+,-,*,/]
+    list_result=[]
+    j=0
+    for i in range(sik):
+        if sik[0]!=0:
+            while j==sik[0]:
+                list_dustkswk.append("+")
+                j+=1
+        if sik[1]!=0:
+            while j==suj[1]
+    for i in range(len(num)-1):
+        for j in range(list_dustkswk):
+            
+            
+            
+
+
+n=int(input())
+num=list(map(int,input().split()))
+sik=list(map(int,input().split()))
+
+make_sik(num,sik)

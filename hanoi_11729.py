dafd28611ef8b0abce4ff973b96713efa5e87848
Mercurius-0227 <10rladmsql@naver.com>
2020-08-07 19:57:40 +0900

    clear not my

--- hanoi_11729.py
+++ hanoi_11729.py
@@ -1,11 +1,11 @@
+def hanoi(원반개수, 시작, 목표, 보조):
+    if 원반개수==1:
+        print(시작, '->', 목표)
+        return
+    hanoi(원반개수-1, 시작, 보조, 목표)
+    print(시작, '->', 목표)
+    hanoi(원반개수-1, 보조, 목표, 시작)
+
+
 n=int(input())
-i=0
-top=[]
-while i<n:
-    num=int(input())
-    if num:
-        top.append(num)
-    else:
-        top.pop()
-    i+=1
-print(sum(top))
+hanoi(n,1,3,2)
